018ec4b633bc427fd1b37329670203395641b862
anupghosh-git <98044386+anupghosh-git@users.noreply.github.com>
2022-01-20 21:26:42 +0100

    Master (#2)
    * First commit to git.
    
    * First commit to git.
    
    * First commit to git.
    
    * First commit to git.
    
    * First commit to git.
    
    * Preparation file commit with play board display function.
    
    Co-authored-by: Anup Ghosh <anup.duet@gmail.com>

---
+++ preperation.py
@@ -0,0 +1,25 @@
+play_board = [
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3],
+    [1,8,9,7,0,0,0,1,3]
+]
+
+def display_playboard(pbo):
+    for i in range(len(pbo)):
+        if i % 3 == 0 and i != 0:
+            print("--------------------------------------")
+        for j in range(len(pbo[0])):
+            if j % 3 == 0 and j != 0:
+             print(" | ", end = "")
+            if j==8:
+              print(pbo[i][j])
+            else:
+                 print(str(pbo[i][j]) + " ", end = "")
+
+display_playboard(play_board)
